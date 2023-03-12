a96b8c6a08c9d2e9e603879d8346f6ead57b9034
skymotic <lobr266238@gmail.com>
2023-03-12 03:32:23 +0000

    Added error checking to scripts and added author info

--- docker-backup.sh
+++ docker-backup.sh
@@ -1,12 +1,68 @@
 #!/bin/bash
-ContainerName=$1
-AllVol=$(docker inspect $ContainerName | grep Source | sed -rn 's|.*/(.*)/_data.*|\1|p')
-mkdir $(pwd)/${ContainerName}-Dir
-for x in $AllVol; do
-	VolMount=$(docker volume inspect --format '{{.Mountpoint}}' $x)
-	7z a $(pwd)/${ContainerName}-Dir/${x}.7z ${VolMount}/*
-done
-ContainerImage=$(docker inspect $ContainerName | sed -rn 's|.*"Image".*"(.*/.*)".*|\1|p')
-docker save $ContainerImage -o $(pwd)/${ContainerName}-Dir/${ContainerName}.tar
-7z a $(pwd)/${ContainerName}.7z $(pwd)/${ContainerName}-Dir/*
-rm -r $(pwd)/${ContainerName}-Dir
+
+#########################################################################################
+# Program Data:										#
+#											#
+#	Author:		Luke O'Brien							#
+#	Updated:	3/11/2023							#
+#	Run as SU?:	YES								#
+#											#
+#	Reqired Packages:								#
+#		p7zip-full								#
+#											#
+#	Description:									#
+#		The following program will extract all the data from a exsisting	#
+#		Docker container's image and volumes and export it into a sinlge 	#
+#		'.7z' file								#
+#											#
+#	Why 7zip?:									#
+#		I had chose to use 7zip instead of tar or gzip because 7zip 		#
+#		offers a higher compression ratio and faster speeds a lot of		#
+#		the time.								#
+#											#
+#########################################################################################
+
+# Pulls the names of all active Docker containers into 'temp'
+TEMP=$(docker ps -a --format "{{.Names}}")
+
+# Check to see if there is input; error out if not
+if [ $# -eq 0 ]; then
+	echo "[-] No argument input..."
+	echo "[ ] Please Enter the name of the container you want to backup"; echo
+	exit 1
+
+# Checks the input to see if Docker container with that name exsists
+elif $(echo $TEMP | grep -Fq "$1"); then
+
+	# Defines Container name for rest of script
+	ContainerName=$1
+	
+	# Captures each volume used by line in 'AllVol'
+	AllVol=$(docker inspect $ContainerName | grep Source | sed -rn 's|.*/(.*)/_data.*|\1|p')
+
+	mkdir $(pwd)/${ContainerName}-Dir
+
+	# Loops through each volume, mapping the path to the data and archiving it
+	# into a .7z file placed in the directory made for the backup
+	for x in $AllVol; do
+		VolMount=$(docker volume inspect --format '{{.Mountpoint}}' $x)
+		7z a $(pwd)/${ContainerName}-Dir/${x}.7z ${VolMount}/*
+	done
+
+	# Captures the image name used by the container
+	ContainerImage=$(docker inspect $ContainerName | sed -rn 's|.*"Image".*"(.*/.*)".*|\1|p')
+
+	# Exports Image to temp backup directory
+	docker save $ContainerImage -o $(pwd)/${ContainerName}-Dir/${ContainerName}.tar
+
+	# Archives each volume and the image into a single 7zip file
+	7z a $(pwd)/${ContainerName}.7z $(pwd)/${ContainerName}-Dir/*
+
+	# Removes the temp directory
+	rm -r $(pwd)/${ContainerName}-Dir
+
+# Throws error if container name doesn't match exsisting Docker containers
+else
+	echo
+	echo "[-] ${1}, is not an exsisting container..."; echo
+fi
--- docker-restore.sh
+++ docker-restore.sh
@@ -1,20 +1,70 @@
 #!/bin/bash
-ContainerName=$(echo $1 | sed -rn 's|(.*)\.7z|\1|p')
-7z x $(pwd)/$1 -o$(pwd)/${ContainerName}-Dir/
-for x in $(pwd)/${ContainerName}-Dir/*.7z; do
-	VolName=$(echo $x | sed -rn 's|.*/(.*)\.7z|\1|p')
-	docker volume create $VolName
-	VolPath=$(docker volume inspect --format '{{.Mountpoint}}' $VolName)
-	7z x -o${VolPath}/ $x
-	chown -R 1000:1000 ${VolPath}
-	chmod -R 755 ${VolPath}
-done
 
-docker load -i $(pwd)/${ContainerName}-Dir/*.tar
-rm -r $(pwd)/${ContainerName}-Dir
+#########################################################################################
+# Program Data:                                                                         #
+#                                                                                       #
+#       Author:         Luke O'Brien                                                    #
+#       Updated:        3/11/2023                                                       #
+#       Run as SU?:     YES                                                             #
+#                                                                                       #
+#       Reqired Packages:                                                               #
+#               p7zip-full                                                              #
+#                                                                                       #
+#       Description:									#
+#		Program will take an exsisting backup archive and restore the		#
+#		volumes and image to docker engine on host				#
+#                                                                                       #
+#       Why 7zip?:                                                                      #
+#               I had chose to use 7zip instead of tar or gzip because 7zip             #
+#               offers a higher compression ratio and faster speeds a lot of            #
+#               the time.                                                               #
+#                                                                                       #
+#########################################################################################
 
-echo "----- ----- ----- -----"
-echo "The Image, ${ContainerName}, and all its volumes have been created and uploaded to the system$(echo)"
-echo "Now you must manually start the container"
-echo
-echo "----- ----- ----- -----"
+
+# Checks to see if there is input; erroring out if not
+if [ $# -eq 0 ]; then
+	echo "[-] No argument input..."
+	echo "[ ] Please enter the filepath of the .7z container backup!"; echo
+	exit 1
+
+# Checks the input to make sure the file exsists
+elif [ -f "$1" ]
+
+	# Defines the name of the container based of file name
+	ContainerName=$(echo $1 | sed -rn 's|(.*)\.7z|\1|p')
+
+	# Extracts contents of 7zip file into a temporary directory
+	7z x $1 -o$(pwd)/${ContainerName}-Dir/
+
+	# Loops through all 7zip files just extracted into temp Directory
+	for x in $(pwd)/${ContainerName}-Dir/*.7z; do
+		
+		# Defines volume name based on file name before creating volume in docker
+		VolName=$(echo $x | sed -rn 's|.*/(.*)\.7z|\1|p')
+		docker volume create $VolName
+
+		# Defines mount point of volume and extracts all contents of 7z there.
+		# Then sets file permisions to default user
+		VolPath=$(docker volume inspect --format '{{.Mountpoint}}' $VolName)
+		7z x -o${VolPath}/ $x
+		chown -R 1000:1000 ${VolPath}
+		chmod -R 755 ${VolPath}
+	done
+
+	# Loads container image into docker
+	docker load -i $(pwd)/${ContainerName}-Dir/*.tar
+
+	# Removes temp directory
+	rm -r $(pwd)/${ContainerName}-Dir
+	
+	# Displays Process and gives user further instructions
+	echo "----- ----- ----- -----"
+	echo "The Image, ${ContainerName}, and all its volumes have been created and uploaded to the system$(echo)"
+	echo "Now you must manually start the container"
+	echo
+	echo "----- ----- ----- -----"
+
+# Errors out if File does not exsist
+else
+	echo "[-] Files doesn't exsist"
